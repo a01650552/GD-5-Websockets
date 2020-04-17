2c66c0b5937ff2fefe389c4d5bf824bb34584315
Alonso <a01650552@itesm.mx>
2020-04-17 15:52:54 -0500

    calculo de resultados y mensaje de resultados

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
+let playerStatus;
+let playerId;
+
 
 function showToast(msg) {
-    console.log("El mensaje es: ", msg);
     $.toast({
         text: msg,
         position: "top-right"
@@ -8,21 +10,115 @@ function showToast(msg) {
 }
 
 window.socket = null;
+
 function connectToSocketio() {
     let server = window.location.protocol + "//" + window.location.host;
     window.socket = io.connect(server);
-
     window.socket.on('toast', function(data){
         showToast(data.message);
     })
 
     window.socket.on('player', function(data){
-        document.getElementById("usuario").innerHTML = `Bienvenido a Basta, ${data}`; 
+        playerId = data.id;
+        playerStatus = data.status;
+        document.getElementById("usuario").innerHTML = `Bienvenido a Basta, ${playerId}`; 
+        
+        if(data.status === false){
+            document.getElementById("juego").hidden = true; 
+            document.getElementById("status").innerHTML = `El juego está lleno. ¡Espera tu turno!`; 
+        } else {
+            /*
+            document.getElementById("status").innerHTML = `¡A jugar!`; 
+            */
+        }
     })
+
+    window.socket.on('init', function(data){
+        initialState()
+    })
+
+    window.socket.on('showGame', function(data){
+        showGame()
+    })
+
+    window.socket.on('startGame', function(data){
+        //startGame()
+        document.getElementById("status").innerHTML = `La letra es ${data.letter}. ¡A jugar!`; 
+        document.getElementById("start"). hidden = true;
+        document.getElementById("btnBasta").hidden = false;
+    })
+
+    window.socket.on("basta", function(data){
+        document.getElementById("btnBasta").hidden = true;
+        startCount()
+    })
+
+    window.socket.on("resultado", function(data){
+        showResults(data.winner, data.points)
+    })
+
 }
+
 
 function messageToServer(msg) {
     window.socket.emit('message-to-server', {message: msg});
+}
+
+function initialState() {
+    document.getElementById("juego").hidden = true; 
+    document.getElementById("status").innerHTML = `Eres el primer jugador, ¡espera a tu oponente!`; 
+}
+
+function showGame() {
+    document.getElementById("juego").hidden = false;
+    document.getElementById("nombre").value = '';
+    document.getElementById("color").value = '';
+    document.getElementById("fruto").value = '';
+    document.getElementById("status").innerHTML = ``;
+    document.getElementById("start"). hidden = false;
+    document.getElementById("btnBasta").hidden = true; 
+}
+
+function startGame() {
+    window.socket.emit('startGame');
+}
+
+function basta() {
+    window.socket.emit('basta')
+}
+
+function startCount() {
+    document.getElementById('status').innerHTML = "¡Alguien dijo Basta!";
+    var i = 10;
+    var time = setInterval(function() {
+        showToast(i);
+        i--;
+        if(i < 0){
+            clearInterval(time);
+            sendAnswers();
+        }
+    }, 1000);
+}
+
+function sendAnswers() {
+    var nombre = document.getElementById("nombre").value;
+    var color = document.getElementById("color").value;
+    var fruto = document.getElementById("fruto").value;
+    window.socket.emit('sendAnswers', {id: playerId, nombre: nombre, color: color, fruto: fruto})
+}
+
+function showResults(winner, points) {
+    document.getElementById("nombre").value = '';
+    document.getElementById("color").value = '';
+    document.getElementById("fruto").value = '';
+    if(winner === true){
+        document.getElementById("status").innerHTML = `¡Ganaste! Obtuviste ${points} puntos. Buen trabajo.`; 
+    }
+    else if(winner === "tie"){
+        document.getElementById("status").innerHTML = `¡Empate!. Obtuviste ${points} puntos. Buen juego.`;
+    } else {
+        document.getElementById("status").innerHTML = `Perdiste. Obtuviste ${points} puntos. ¡Mejor suerte la próxima!`;
+    }
 }
 
 $(function () {
--- server.js
+++ server.js
@@ -65,7 +65,13 @@ const result = {
 const gameStatus = {
   FINISHED: true,
   NOTFINISHED: false,
-  NOTSTARTED: false
+  NOTSTARTED: "notstarted"
+}
+
+const playerStatus = {
+  ACTIVE: true,
+  LOBBY: false,
+  FIRST: "first"
 }
 
 //clase jugador
@@ -76,10 +82,10 @@ class Player {
       this.opponent = 'unmatched';
       this.points = 0;
       this.result = result.NS;
-  }
-
-  setOpponent(opponent) {
-    this.opponent = opponent;
+      this.status = playerStatus.FIRST;
+      this.color = '';
+      this.fruto = '';
+      this.nombre = '';
   }
 }
 
@@ -87,26 +93,123 @@ class Player {
 class Board {
   constructor(){
     this.players = [];
-    this.letter = null;
     this.status = gameStatus.NOTSTARTED;
+    this.lobby = [];
+    this.letter = '';
+    this.answersRecieved = 0;
   }
 
   getLetter(){
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    var alphabet = 'ABCDEFGHIJKLMNOPQRZTUVWXYZ';
     var position = Math.floor(Math.random() * alphabet.length)
-    this.randomLetter = letters.charAt(position);
+    this.randomLetter = alphabet.charAt(position);
+    this.letter = this.randomLetter;
     return this.randomLetter;
   }
 
    addPlayer(player) {
     if (this.players.length < 2) {
+      player.status = playerStatus.ACTIVE;
       this.players.push(player);
       if(this.players.length == 2) {
         this.players[0].opponent = this.players[1];
         this.players[1].opponent = this.players[0];
       }
     }
+    else {
+      player.status = playerStatus.LOBBY;
+      this.lobby.push(player);
+    }
    }
+   
+   addFromLobby(){
+    for(var i = 0; i < this.lobby.length; i++) {
+      //Si no hay dos jugadores activos, se manda al primer jugador de la lista de espera a la lista de jugadores activos
+      if (this.players.length < 2) {
+        this.addPlayer(this.lobby.shift());
+      }
+    }
+   }
+
+   deletePlayer(id) {
+    for(var i = 0; i < this.players.length; i++) {
+      if(this.players[i].id == id) {
+        this.players.splice(i, 1);
+        this.addFromLobby();
+      }
+    }
+    for(var i = 0; i < this.lobby.length; i++) {
+      if(this.lobby[i].id == id) {
+        this.lobby.splice(i, 1);
+      }
+    }
+  }
+  
+  getResults() {
+    console.log("Jugador 0: ", this.players[0].id);
+    console.log("Jugador 1: ", this.players[1].id);
+
+    console.log("Jugador 0: ", this.players[0].nombre);
+    console.log("Jugador 1: ", this.players[1].nombre);
+
+    if(this.players[0].nombre == this.players[1].nombre && this.players[0].nombre.charAt(0) == this.letter){
+      this.players[0].points = this.players[0].points + 5;
+      this.players[1].points = this.players[1].points + 5;
+      console.log("Jugador 0, nombre: ", this.players[0].points)
+      console.log("Jugador 1, nombre: ", this.players[1].points)
+    }
+    if(this.players[0].color == this.players[1].color && this.players[0].color.charAt(0) == this.letter){
+      this.players[0].points = this.players[0].points + 5;
+      this.players[1].points = this.players[1].points + 5;
+      console.log("Jugador 0, color: ", this.players[0].points)
+      console.log("Jugador 1, color: ", this.players[1].points)
+    }
+    if(this.players[0].fruto == this.players[1].fruto && this.players[0].fruto.charAt(0) == this.letter){
+      this.players[0].points = this.players[0].points + 5;
+      this.players[1].points = this.players[1].points + 5;
+    }
+    if(this.players[0].nombre != this.players[1].nombre){
+      if(this.players[0].nombre.charAt(0) == this.letter){
+        this.players[0].points = this.players[0].points + 10;
+      }
+      if(this.players[1].nombre.charAt(0) == this.letter){
+        this.players[1].points = this.players[1].points + 10;
+      }
+      console.log("Jugador 0, nombre: ", this.players[0].points)
+      console.log("Jugador 1, nombre: ", this.players[1].points)
+    }
+    if(this.players[0].color != this.players[1].color){
+      if(this.players[0].color.charAt(0) == this.letter){
+        this.players[0].points = this.players[0].points + 10;
+      }
+      if(this.players[1].color.charAt(0) == this.letter){
+        this.players[1].points = this.players[1].points + 10;
+      }
+      console.log("Jugador 0, color: ", this.players[0].points)
+      console.log("Jugador 1, color: ", this.players[1].points)
+    }
+    if(this.players[0].fruto != this.players[1].fruto){
+      if(this.players[0].fruto.charAt(0) == this.letter){
+        this.players[0].points = this.players[0].points + 10;
+      }
+      if(this.players[1].fruto.charAt(0) == this.letter){
+        this.players[1].points = this.players[1].points + 10;
+      }
+      console.log("Jugador 0, fruto: ", this.players[0].points)
+      console.log("Jugador 1, fruto: ", this.players[1].points)
+    }
+
+  }
+
+  getWinner(){
+    if(this.players[0].points > this.players[1].points){
+      return 0;
+    } else if(this.players[0].points < this.players[1].points){
+      return 1;
+    } else {
+      return 2;
+    }
+  }
   
 }
 
@@ -118,14 +221,84 @@ io.on('connection', (socket) => {
   basta.addPlayer(player);  
 
   socket.emit('toast', {message: `Bienvenido ${player.id}`});
-  console.log(`Conectado: ${player.id}`)
+  console.log(`Connected: ${player.id}`)
 
-  socket.emit('player', player.id);
+  socket.emit('player', {id: player.id, status: player.status});
+
+  if(basta.players == 2){
+    socket.emit('hideGame');
+  }
+
+  if (basta.players.length == 2){
+    for(i = 0; i < basta.players.length; i++){
+      basta.players[i].socket.emit('showGame');
+    }
+  }
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected: ", socket.id);
+    basta.deletePlayer(socket.id);
+    if(basta.players.length > 0){
+      if(basta.players.length == 1){
+        basta.players[0].socket.emit('init');
+      } else{
+        for(i = 0; i < basta.players.length; i++){
+          basta.players[i].socket.emit('showGame');
+        }
+      }
+    }
+  });
+
+  socket.on("startGame", () => {
+    var letter = basta.getLetter();
+    for(i = 0; i < basta.players.length; i++){
+      basta.players[i].socket.emit('startGame', {letter: letter});
+    }
+  })
+
+  socket.on("basta", (data) => {
+    for(i = 0; i < basta.players.length; i++){
+      basta.players[i].socket.emit('basta');
+    }
+  })
+
+  socket.on("sendAnswers", (data) => {
+    var nombre = data.nombre.toUpperCase();
+    var fruto = data.fruto.toUpperCase();
+    var color = data.color.toUpperCase();
+    console.log("ID: ", data.id);
+    if(basta.players[0].id === data.id){
+      basta.players[0].nombre = nombre;
+      basta.players[0].fruto = fruto;
+      basta.players[0].color = color;
+    } else {
+      basta.players[1].nombre = nombre;
+      basta.players[1].fruto = fruto;
+      basta.players[1].color = color;
+    }
+    basta.answersRecieved = basta.answersRecieved + 1;
+    if(basta.answersRecieved == 2){
+      basta.getResults();
+      var ganador = basta.getWinner();
+      if(ganador < 2){
+        for(i = 0; i < basta.players.length; i++){
+          if(ganador == i){
+            basta.players[i].socket.emit('resultado', {points: basta.players[i].points, winner: true});
+          } else{
+            basta.players[i].socket.emit('resultado', {points: basta.players[i].points, winner: false});
+          }
+        }
+      } else{
+        basta.players[0].socket.emit('resultado', {points: basta.players[0].points, winner: "tie"});
+        basta.players[1].socket.emit('resultado', {points: basta.players[1].points, winner: "tie"});
+      }
+    }
+  })
 
   socket.on('message-to-server', (data) => {
     console.log('message received', data);
   });
-  
+
 });
 
 // App init
